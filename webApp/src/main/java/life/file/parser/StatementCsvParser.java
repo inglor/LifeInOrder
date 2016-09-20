c4612e214b24c613702734960d9d7a219515df51
Leonidas Spyropoulos <artafinde@gmail.com>
2016-09-20 23:27:36 +0100

    Use univocity and regex for speed
    Signed-off-by: Leonidas Spyropoulos <artafinde@gmail.com>

--- webApp/src/main/java/life/file/parser/StatementCsvParser.java
+++ webApp/src/main/java/life/file/parser/StatementCsvParser.java
@@ -2,17 +2,18 @@ package life.file.parser;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import au.com.bytecode.opencsv.CSVParser;
-import au.com.bytecode.opencsv.CSVReader;
 import life.database.model.BankTransaction;
 import life.file.DateUtilsImpl;
 
@@ -20,7 +21,18 @@ import life.file.DateUtilsImpl;
 @Transactional
 public class StatementCsvParser implements FileParser<MultipartFile, List<BankTransaction>> {
   private static final Logger LOG = LoggerFactory.getLogger(StatementCsvParser.class);
-  private DateUtilsImpl dateUtils = new DateUtilsImpl();
+  private static final String STATEMENT_CSV_REGEX = "(\\d{1,2}/\\d{1,2}/\\d{2},[a-zA-Z0-9].*,-?\\d+.\\d+)";
+  private static final String MIDATA_CSV_REGEX = "(\\d{2}/\\d{2}/\\d{4},.{2,},[\\w\\s\\W]*?,[+|-].\\d+.\\d+,[+|-].\\d+.\\d+)";
+  private static final Predicate<String[]> STATEMENT_FILTER = f -> Pattern.compile(STATEMENT_CSV_REGEX).matcher(String.join(",", f)).find();
+  private static final Predicate<String[]> MIDATA_FILTER = f -> Pattern.compile(MIDATA_CSV_REGEX).matcher(String.join(",", f)).find();
+  private final DateUtilsImpl dateUtils = new DateUtilsImpl();
+  private final CsvParser parser;
+
+  StatementCsvParser() {
+    CsvParserSettings settings = new CsvParserSettings();
+    settings.getFormat().setLineSeparator("\n");
+    parser = new CsvParser(settings);
+  }
 
   /**
    * This will read the file and check if the headers are 3 or 5 columns which correspond to
@@ -31,9 +43,9 @@ public class StatementCsvParser implements FileParser<MultipartFile, List<BankTr
    */
   @Override
   public boolean canParse(MultipartFile multipartFile) {
-    try (CSVReader reader = new CSVReader(new InputStreamReader(multipartFile.getInputStream()),
-        CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_QUOTE_CHARACTER, 1)) {
-      String[] headers = reader.readNext();
+    try {
+      parser.parse(multipartFile.getInputStream());
+      String[] headers = parser.parseNext();
       if (headers.length != 3 && headers.length != 5) {
         return false;
       }
@@ -61,15 +73,14 @@ public class StatementCsvParser implements FileParser<MultipartFile, List<BankTr
    */
   @Override
   public List<BankTransaction> parse(MultipartFile multipartFile) {
-    try (CSVReader reader = new CSVReader(new InputStreamReader(multipartFile.getInputStream()),
-        CSVParser.DEFAULT_SEPARATOR, CSVParser.DEFAULT_QUOTE_CHARACTER, 1)) {
-      List<String[]> lines = reader.readAll();
+    try {
+      List<String[]> lines = parser.parseAll(multipartFile.getInputStream());
       return Stream.concat(
           lines.stream()
-              .filter(line -> line.length == 5)
+              .filter(MIDATA_FILTER)
               .map(line -> new BankTransaction(dateUtils.convertTextToDate(line[0]), line[2], getValue(line[3]))),
           lines.stream()
-              .filter(line -> line.length == 3)
+              .filter(STATEMENT_FILTER)
               .map(line -> new BankTransaction(dateUtils.convertTextToDate(line[0]), line[1], getValue(line[2])))
       ).collect(Collectors.toList());
     } catch (IOException e) {
