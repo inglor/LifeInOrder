f7a7c686bfb4414ee98aede1c709ea4b6f7a49d0
Leonidas Spyropoulos <artafinde@gmail.com>
2016-09-20 08:51:21 +0100

    Inject on constructor and check for null
    Signed-off-by: Leonidas Spyropoulos <artafinde@gmail.com>

--- webApp/src/main/java/life/web/controller/BankingFacade.java
+++ webApp/src/main/java/life/web/controller/BankingFacade.java
@@ -1,29 +1,25 @@
 package life.web.controller;
 
-import java.util.List;
+import static com.google.common.base.Preconditions.checkNotNull;
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import life.database.dao.BankTransactionDao;
+import java.util.List;
 import life.database.model.BankTransaction;
 
 @Named
 class BankingFacade {
 
-  private BankTransactionDao bankTransactionDao;
   private BankService bankService;
 
   @Inject
-  public BankingFacade(BankTransactionDao bankTransactionDao, BankService bankService) {
-    this.bankTransactionDao = bankTransactionDao;
-    this.bankService = bankService;
+  public BankingFacade(BankService bankService) {
+    this.bankService = checkNotNull(bankService);
   }
 
   void save(List<BankTransaction> bankTransactions) {
     bankService.saveBankTransactions(bankTransactions);
-    for(BankTransaction bankTransaction : bankTransactions) {
+    for (BankTransaction bankTransaction : bankTransactions) {
       bankService.setMonthStat(bankTransaction);
     }
   }
-
 }
