49ae801ddd957933dfe03460ec56b904074ef948
Leonidas Spyropoulos <artafinde@gmail.com>
2016-10-01 09:35:48 +0100

    Simplify converter
    Signed-off-by: Leonidas Spyropoulos <artafinde@gmail.com>

--- webApp/src/main/java/life/database/converter/YearMonthAttributeConverter.java
+++ webApp/src/main/java/life/database/converter/YearMonthAttributeConverter.java
@@ -1,26 +1,21 @@
 package life.database.converter;
 
+import javax.annotation.Nullable;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.sql.Date;
-import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.Calendar;
 
 @Converter(autoApply = true)
 public class YearMonthAttributeConverter implements AttributeConverter<YearMonth, Date> {
 
   @Override
-  public Date convertToDatabaseColumn(YearMonth yearMonth) {
-    LocalDate localDate = LocalDate.of(yearMonth.getYear(), yearMonth.getMonth(), 1);
-    return (yearMonth == null ? null : Date.valueOf(localDate));
+  public Date convertToDatabaseColumn(@Nullable YearMonth yearMonth) {
+    return (yearMonth == null ? null : Date.valueOf(yearMonth.atDay(1)));
   }
 
   @Override
-  public YearMonth convertToEntityAttribute(Date sqlDate) {
-    Calendar cal = Calendar.getInstance();
-    cal.setTime(sqlDate);
-    YearMonth yearMonth = YearMonth.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1);
-    return (sqlDate == null ? null : yearMonth);
+  public YearMonth convertToEntityAttribute(@Nullable Date sqlDate) {
+    return (sqlDate == null ? null : YearMonth.from(sqlDate.toLocalDate()));
   }
 }
